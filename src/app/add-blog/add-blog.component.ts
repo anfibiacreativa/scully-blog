53f2a551e4a4c4ea561467a4d799f010370aa936
Natalia Venditto <nataliafrontend@gmail.com>
2020-05-14 21:44:39 +0200

    update path to constants

--- src/app/add-blog/add-blog.component.ts
+++ src/app/add-blog/add-blog.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { initConfig } from '../../commons/constants/constants';
+import { initConfig } from '../commons/constants/constants';
 
 @Component({
   selector: 'app-add-blog',
