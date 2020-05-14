ad7f04ec498d5b2817879dce96416c46e582ee02
Natalia Venditto <nataliafrontend@gmail.com>
2020-05-14 21:44:03 +0200

    add url for service to constants

---
+++ src/app/commons/constants/constants.ts
@@ -0,0 +1,10 @@
+export const blogRoot = 'blog';
+export const initConfig = `
+--- \n
+title: \n
+description: \n
+published: false \n
+slugs: \n
+--- \n
+`;
+export const api_url = 'http://demo7107977.mockable.io/content/blog/entries';
